9807a65150c44bb97286e2863fa4bf1330baaacf
Noufel sf <noufel_seif_el_islam.nasri@univ-constantine2.dz>
2026-03-04 11:43:39 +0100

    adding update new members registerations status

--- routes/NewMemberRegesterationRoutes.ts
+++ routes/NewMemberRegesterationRoutes.ts
@@ -4,6 +4,7 @@ import {
   getRegistrations,
   getRegistration,
   deleteRegistration,
+  updateRegistrationStatus,
 } from "../Controllers/NewMemberRegesterationController";
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.post("/", createRegistration);
 router.get("/", getRegistrations);
 router.get("/:id", getRegistration);
+router.patch("/:id/status", updateRegistrationStatus);
 router.delete("/:id", deleteRegistration);
 
 export default router;
